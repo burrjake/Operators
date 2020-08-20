73b886cd26181e377fb31a039f537350a90f0510
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-20 08:46:18 -0600

    add operators

--- Operators/Module1.vb
+++ Operators/Module1.vb
@@ -1,7 +1,18 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+        Console.WriteLine(2 - 2)
+        Console.ReadLine()
+        Console.WriteLine(2 * 2)
+        Console.ReadLine()
+        Console.WriteLine(2 / 2)
+        Console.ReadLine()
+        Console.WriteLine(2 \ 2)
+        Console.ReadLine()
+        Console.WriteLine(2 Mod 2)
+        Console.ReadLine()
     End Sub
 
 End Module
