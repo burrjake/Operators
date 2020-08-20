179593059b38cd01b3e0d9cef6d0457c4a23fe5f
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-20 08:56:46 -0600

    add concatenate

--- Operators/Module1.vb
+++ Operators/Module1.vb
@@ -13,6 +13,11 @@
         Console.ReadLine()
         Console.WriteLine(2 Mod 2)
         Console.ReadLine()
+
+        'add concatenate
+        Console.WriteLine("Hello" & "," & " " & "World")
+        Console.ReadLine()
+
     End Sub
 
 End Module
